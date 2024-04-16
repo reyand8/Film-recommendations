014eb5dd1e063a57863d87f6ec59515850a636f3
Andrey <andreeey.gg@gmail.com>
2024-04-16 13:22:14 +0200

    add config file

---
+++ server/src/config/index.js
@@ -0,0 +1,9 @@
+const API_BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'YOUR_KEY';
+const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/w300'
+
+module.exports = {
+    API_BASE_URL,
+    API_KEY,
+    IMAGE_BASE_PATH,
+}
