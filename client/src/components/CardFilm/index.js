7a2f3ba6929cacb469aee3c9d28bc2e882aed3da
Andrey <andreeey.gg@gmail.com>
2024-04-16 13:05:02 +0200

    add link to the Single page and moving getVoteCicle, changeColor to common.js

--- client/src/components/CardFilm/index.js
+++ client/src/components/CardFilm/index.js
@@ -1,15 +1,18 @@
-import * as React from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress'
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
 import {Box, CardContent} from "@mui/material";
 
 import adultIcon from "../../assets/img/adult.svg"
 import theme from "../../assets/theme";
-import CircularProgress from '@mui/material/CircularProgress'
+import {changeColor, getVoteCicle} from "../../common";
+
 
 const CardInfo = styled(CardContent)(({theme}) => ({
     '&:last-child': {
@@ -41,24 +44,8 @@ const PlusIcon = styled(Box)(({ theme }) => ({
 }))
 
 export const CardFilm = ({film, onCardSelect, isPreviewMode}) => {
-    const {title, image, releaseDate, adult, voteAverage } = film
-    const voteCicle = voteAverage * 10
-
-    const getVoteCicle = (vote) => {
-        if (vote <= 100) {
-            return voteCicle.toFixed(1)
-        }
-    }
-
-    const changeColor = (vote) => {
-        if (vote > 66.67) {
-            return theme.palette.success.main
-        } else if (vote > 33.34) {
-            return theme.palette.warning.light
-        }else {
-            return theme.palette.error.light
-        }
-    }
+    const {id, title, image, releaseDate, adult, voteAverage } = film
+    const voteCicle = (voteAverage * 10)
 
     return (
         <Card sx={{ maxWidth: 250, height: '460px', position: "relative" }}>
@@ -67,54 +54,41 @@ export const CardFilm = ({film, onCardSelect, isPreviewMode}) => {
                     <CircularProgress
                         sx={{
                         background: theme.palette.background.paper,
-                        borderRadius: '50%',
-                        color: (`${changeColor(voteCicle)}`)}}
-                        size={58}
-                        variant="determinate"
-                        value={voteCicle} />
-                    <Box
-                        top={0}
-                        left={0}
-                        bottom={0}
-                        right={0}
-                        position="absolute"
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center">
+                        borderRadius: '50%', color: (`${changeColor(voteCicle)}`)}}
+                        size={58} variant="determinate" value={voteCicle} />
+                    <Box top={0} left={0} bottom={0} right={0}
+                        position="absolute" display="flex"
+                        alignItems="center" justifyContent="center">
                         <Typography variant="subtitle2" component="div" color="textSecondary">
                             {`${getVoteCicle(voteCicle)}%`}
                         </Typography>
                     </Box>
                 </Box>
-                <CardMedia
-                    component="img"
-                    height="320"
-                    image={image}
-                    alt={title}/>
+                <CardMedia component="img" height="320" image={image} alt={title}/>
                     {!isPreviewMode && (
                     <PlusIcon onClick={() => onCardSelect(film)}>
                         <AddBoxOutlinedIcon sx={{ fontSize: 80, fill: theme.palette.secondary.dark}}/>
                     </PlusIcon>
                     )}
             </Box>
-            <CardInfo>
-                <Typography variant="h5" gutterBottom component="div">
-                    {title}
-                </Typography>
-                <Box sx={{ display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center'}}>
-                    <Typography mb={0} variant="subtitle1" gutterBottom component="div">
-                        {releaseDate}
+            <Link to={`/film/${id}`} key={id}>
+                <CardInfo>
+                    <Typography variant="h5" gutterBottom component="div">
+                        {title}
                     </Typography>
-                    <Typography mb={0}>
-                        {!adult ? (
-                            <img className='icon' src={adultIcon} alt=""/>
-                        ): 'new'}
-                    </Typography>
-                </Box>
-            </CardInfo>
+                    <Box sx={{ display: 'flex', flexDirection: 'row',
+                        justifyContent: 'space-between', alignItems: 'center'}}>
+                        <Typography mb={0} variant="subtitle1" gutterBottom component="div">
+                            {releaseDate}
+                        </Typography>
+                        <Typography mb={0}>
+                            {!adult ? (
+                                <img className='icon' src={adultIcon} alt=""/>
+                            ) : 'new'}
+                        </Typography>
+                    </Box>
+                </CardInfo>
+            </Link>
         </Card>
     )
 }
@@ -123,7 +97,9 @@ CardFilm.propTypes = {
     film: PropTypes.shape({
         image: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
-        releaseDate: PropTypes.string
+        releaseDate: PropTypes.string,
+        adult: PropTypes.bool,
+        voteAverage: PropTypes.number,
     }).isRequired,
     onCardSelect: PropTypes.func
 }
