e002540435aa0cf80577e142235b3a66b51fc93d
Andrey <andreeey.gg@gmail.com>
2024-04-18 17:54:52 +0200

    add new routes

--- client/src/components/AppRoutes/AppRoutes.js
+++ client/src/components/AppRoutes/AppRoutes.js
@@ -4,15 +4,19 @@ import {ROUTES} from "../../routes";
 import {Home, Recommend, Settings} from "../../pages";
 import SingleFilm from "../../pages/SingleFilm";
 import Genres from "../../pages/Genres";
+import FilmsByGenre from "../../pages/FilmsByGenre";
+import Search from "../../pages/Search";
 
 
 const AppRoutes = () => (
     <Routes>
         <Route index element={<Home />} />
         <Route path={ROUTES.FILM} element={<SingleFilm />} />
-        <Route path={ROUTES.SETTINGS} element={<Settings />}/>
         <Route path={ROUTES.GENRES} element={<Genres />} />
-        <Route path={ROUTES.RECOMMEND} element={<Recommend />}/>
+        <Route path={ROUTES.FILMS_BY_GENRE} element={<FilmsByGenre />} />
+        <Route path={ROUTES.SEARCH} element={<Search />} />
+        <Route path={ROUTES.RECOMMEND} element={<Recommend />} />
+        <Route path={ROUTES.SETTINGS} element={<Settings />} />
     </Routes>
 );
 
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,6 +2,8 @@ export const ROUTES = {
     HOME: '/',
     FILM: '/film/:id',
     GENRES: '/genres',
+    FILMS_BY_GENRE: '/genre/:id',
+    SEARCH: '/search',
     SETTINGS: '/settings',
     RECOMMEND: '/recommend'
 }
