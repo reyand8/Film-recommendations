9a95166566720f0ea96aef7bba5c4bb88a134cf9
Andrey <andreeey.gg@gmail.com>
2024-04-19 12:17:31 +0200

    add component SingleFilm

---
+++ client/src/pages/SingleFilm/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+import {useQuery} from "@apollo/client";
+import {Box, Grid} from "@mui/material";
+
+import DataError from "../../components/DataError";
+import Loading from "../../components/Loading";
+import CardSingleFilm from "../../components/CardSingleFilm";
+import {FILMS_BY_ID_QUERY} from "../../quieries/queries";
+
+
+const SingleFilm = () => {
+    const { id } = useParams();
+
+    const {loading, error, data} = useQuery(FILMS_BY_ID_QUERY, {
+        variables: {
+            ids: +id
+        }
+    })
+
+    if (error) {
+        return <DataError/>;
+    }
+
+    return (
+        <>
+            {loading ? <Loading /> : (
+                <Box>
+                    {data && (
+                        <Grid container spacing={2}>
+                            {data?.filmsById.map((film) => (
+                               <CardSingleFilm film={film}/>
+                            ))}
+                        </Grid>
+                    )}
+                </Box>
+            )}
+        </>
+    )
+}
+
+export default SingleFilm
+
+
+
