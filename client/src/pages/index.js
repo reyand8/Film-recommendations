82d7dbdbd01ec688a251eef66ca09947394f408e
Andrey <andreeey.gg@gmail.com>
2024-04-19 12:17:49 +0200

    add index file for pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from './Home'
+import Settings from './Settings'
+import Recommend from './Recommend'
+import SingleFilm from './SingleFilm'
+import FilmsByGenre from './FilmsByGenre'
+import Search from './Search'
+import Genres from './Genres'
+
+export {
+    Home,
+    Settings,
+    Recommend,
+    SingleFilm,
+    FilmsByGenre,
+    Search,
+    Genres,
+}
