2ccc4cacb917952f502ff643f53e1532e97dbe02
Andrey <andreeey.gg@gmail.com>
2024-04-19 11:53:12 +0200

    add useFilters

---
+++ client/src/hooks/useFilters/index.js
@@ -0,0 +1,34 @@
+import { useState, useCallback } from 'react';
+
+import { SORT_DIRECTION } from '../../const';
+
+
+export const useFilters = () => {
+    const [filter, setFilterFields] = useState({
+        page: 1,
+        sortBy: 'original_title',
+        sortDirection: SORT_DIRECTION.DESC,
+        includeAdult: false,
+    })
+
+    const setPage = useCallback((page) => {
+        setFilterFields({
+            ...filter,
+            page
+        })
+    }, [filter])
+
+    const setFilter = useCallback((filterFields) => {
+        setFilterFields({
+            ...filter,
+            ...filterFields,
+        })
+    }, [filter])
+
+    return {
+        filter,
+        setFilterFields,
+        setPage,
+        setFilter
+    }
+}
