3a0205d33b527b4ec0e9d26fff8ed11ee799946b
Andrey <andreeey.gg@gmail.com>
2024-05-12 15:01:03 +0200

    add SignIn and SignUp

--- client/src/components/account/SignIn/index.js
+++ client/src/components/account/SignIn/index.js
@@ -1,12 +1,10 @@
-import {Box, Grid, Link, TextField} from '@mui/material';
-import Paper from '@mui/material/Paper';
+import {Box, Link, TextField} from '@mui/material';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {styled} from '@mui/material/styles';
 
-const paperStyle={padding :20, minHeight:'36vh', maxWidth: 360, margin:'60px auto'};
 const btnstyle={margin:'8px 0'};
 
 const TextFieldBox = styled(Box)(({ theme }) => ({
@@ -23,35 +21,55 @@ const TextFieldBox = styled(Box)(({ theme }) => ({
 }));
 
 
-const SignIn = () => {
+const SignIn = ({formState, handlerChange, handleSubmit, signIn, setLogin}) => {
     return(
         <>
-            <Grid>
-                <Paper elevation={10} style={paperStyle}>
-                    <Grid align="center">
-                        <h2>Sign In</h2>
-                    </Grid>
-                    <TextFieldBox>
-                        <TextField sx={{mb: '16px'}} label="Username" placeholder="Enter username" variant="outlined" fullWidth required/>
-                        <TextField label="Password" placeholder="Enter password" type="password" variant="outlined" fullWidth required/>
-                        <FormControlLabel
-                            control={<Checkbox name="checkedB" color="primary"/>}
-                            label="Remember me"
-                        />
-                        <Button type="submit" color="primary" variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-                        <Typography sx={{marginY: '12px'}}>
-                            <Link href="#" >
-                                Forgot password ?
-                            </Link>
-                        </Typography>
-                    </TextFieldBox>
-                    <Typography >
-                        <Link href="#" >
-                            Registration
-                        </Link>
-                    </Typography>
-                </Paper>
-            </Grid>
+            <TextFieldBox component="form" onSubmit={handleSubmit}>
+                <TextField
+                    sx={{mb: '16px'}}
+                    label="Username"
+                    placeholder="Enter username"
+                    variant="outlined"
+                    fullWidth required
+                    inputProps={{
+                        pattern: '[A-Za-z ]+',
+                    }}
+                    value={formState.username}
+                    onChange={handlerChange}
+                />
+                <TextField
+                    label="Password"
+                    placeholder="Enter password"
+                    type="password"
+                    variant="outlined"
+                    fullWidth required
+                    value={formState.password}
+                    onChange={handlerChange}
+                />
+                <FormControlLabel
+                    control={<Checkbox name="checkedB" color="primary"/>}
+                    label="Remember me"
+                />
+                <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    style={btnstyle}
+                    fullWidth
+                    onClick={signIn}>
+                    Sign In
+                </Button>
+                <Typography sx={{marginY: '12px'}}>
+                    <Link href="#" >
+                        Forgot password ?
+                    </Link>
+                </Typography>
+            </TextFieldBox>
+            <Typography>
+                <Button onClick={() => setLogin(prev => !prev)}>
+                    Registration
+                </Button>
+            </Typography>
         </>
     );
 };
--- client/src/components/account/SignUp/index.js
+++ client/src/components/account/SignUp/index.js
@@ -1,12 +1,8 @@
-import {Box, Grid, Link, TextField} from '@mui/material';
-import Paper from '@mui/material/Paper';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import {Box, Link, TextField} from '@mui/material';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {styled} from '@mui/material/styles';
 
-const paperStyle={padding :20, minHeight:'42vh', maxWidth: 360, margin:'60px auto'};
 const btnstyle={margin:'8px 0'};
 
 const TextFieldBox = styled(Box)(({ theme }) => ({
@@ -24,28 +20,54 @@ const TextFieldBox = styled(Box)(({ theme }) => ({
 }));
 
 
-const SignUp = () => {
+const SignUp = ({formState, handlerChange, handleSubmit, setLogin}) => {
     return(
         <>
-            <Grid>
-                <Paper elevation={10} style={paperStyle}>
-                    <Grid align="center">
-                        <h2>Sign Up</h2>
-                    </Grid>
-                    <TextFieldBox>
-                        <TextField label="Username" placeholder="Enter username" variant="outlined" fullWidth required/>
-                        <TextField sx={{marginY: '16px'}} label="Email" placeholder="Enter email" variant="outlined" fullWidth required/>
-                        <TextField label="Password" placeholder="Enter password" type="password" variant="outlined" fullWidth required/>
-                        <TextField sx={{marginY: '16px'}} label="Repeat Password" placeholder="Repeat password" type="password" variant="outlined" fullWidth required/>
-                        <Button sx={{mb: '8px'}} type="submit" color="primary" variant="contained" style={btnstyle} fullWidth>Sign up</Button>
-                    </TextFieldBox>
-                    <Typography > Do you have an account ?
-                        <Link href="#" >
-                            Sign In
-                        </Link>
-                    </Typography>
-                </Paper>
-            </Grid>
+            <TextFieldBox component="form" onSubmit={handleSubmit}>
+                <TextField label="Username"
+                           placeholder="Enter username"
+                           name="username"
+                           value={formState.username}
+                           variant="outlined" fullWidth required
+                           inputProps={{
+                               pattern: '[A-Za-z ]+',
+                           }}
+                           onChange={handlerChange}
+                />
+                <TextField sx={{marginY: '16px'}}
+                           label="Email"
+                           placeholder="Enter email"
+                           name="email"
+                           value={formState.email}
+                           variant="outlined" fullWidth required
+                           inputProps={{
+                               type: 'email',
+                           }}
+                           onChange={handlerChange}
+                />
+                <TextField label="Password"
+                           placeholder="Enter password"
+                           type="password"
+                           name="password"
+                           value={formState.password}
+                           variant="outlined" fullWidth required
+                           onChange={handlerChange}
+                />
+                <Button sx={{mb: '8px'}}
+                        type="submit"
+                        color="primary"
+                        variant="contained"
+                        style={btnstyle} fullWidth>
+                    Sign Up
+                </Button>
+            </TextFieldBox>
+            <Typography > Do you have an account ?
+                <Link>
+                    <Button onClick={() => setLogin(prev => !prev)}>
+                        Sign In
+                    </Button>
+                </Link>
+            </Typography>
         </>
     );
 };
