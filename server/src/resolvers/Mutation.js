eca5d0b3cf672757d5925dde7a7673e655d520f0
Andrey <andreeey.gg@gmail.com>
2024-05-31 13:27:53 +0200

    edit updateUser mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const {PrismaClient} = require('@prisma/client')
+const fs = require("fs");
+const path = require("path");
 
 const prisma = new PrismaClient();
 
@@ -45,12 +47,53 @@ async function signInUser (parent, { email, password }, context, info) {
     }
 }
 
-async function updateUser(parent, {email, username}, context) {
+async function updateUser(parent, {email, username, selectedFilms}, context) {
     const userId = context.userId
+    const data = {};
+    if (email) {
+        data.email = email;
+    }
+    if (username) {
+        data.username = username;
+    }
+    if (selectedFilms) {
+        data.selectedFilms = selectedFilms;
+    }
     return prisma.user.update({
         where: {id: userId},
-        data: {email, username}
+        data,
     })
+}
+
+async function updateImage(parent, {file}, context) {
+    try {
+        console.log(file)
+        const userId = context.userId;
+
+        if (!file) {
+            throw new Error('No file provided');
+        }
+        console.log(file)
+        const {createReadStream, filename} = await file;
+        console.log(file)
+        const stream = createReadStream();
+        const filePath = path.join(__dirname, 'uploads', filename);
+
+        await new Promise((resolve, reject) =>
+            stream
+                .pipe(fs.createWriteStream(filePath))
+                .on('finish', resolve)
+                .on('error', reject)
+        );
+        console.log(filePath)
+        return prisma.user.update({
+            where: {id: userId},
+            data: {image: filePath},
+        });
+    } catch (error) {
+        console.error('Error uploading avatar:', error);
+        throw error;
+    }
 }
 
 
@@ -58,4 +101,5 @@ module.exports = {
     signUpUser,
     signInUser,
     updateUser,
+    updateImage,
 }
