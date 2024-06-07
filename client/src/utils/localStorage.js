4f91ec4f71f3e12515e90c58ac5b108ca473593c
Andrey <andreeey.gg@gmail.com>
2024-06-07 14:10:11 +0200

    code refactoring

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -2,22 +2,17 @@ export const saveIdsToStorage = (name, date) => {
     if (!window || !window.localStorage) {
         return;
     }
-
     const arrayId = JSON.parse(window.localStorage.getItem(name)) || [];
-
     if (Array.isArray(date)) {
-        console.log(date);
         date.forEach(el => {
             if (!arrayId.includes(el)) {
                 arrayId.push(el);
             }
         });
     }
-
     if (!arrayId.includes(date) && !Array.isArray(date) ) {
         arrayId.push(date);
     }
-
     window.localStorage.setItem(name, JSON.stringify(arrayId));
 };
 
@@ -25,11 +20,8 @@ export const deleteIdsFromStorage = (name, date) => {
     if (!window || !window.localStorage) {
         return;
     }
-
     const arrayId = JSON.parse(window.localStorage.getItem(name));
-
     const filterArray = arrayId.filter(item => item !== date);
-
     window.localStorage.setItem(name, JSON.stringify(filterArray));
 };
 
@@ -37,7 +29,6 @@ export const saveToStorage = (name, date) => {
     if (!window || !window.localStorage) {
         return;
     }
-
     window.localStorage.setItem(name, JSON.stringify(date));
 };
 
@@ -45,7 +36,6 @@ export const getFromStorage = (name) => {
     if (!window || !window.localStorage) {
         return null;
     }
-
     try {
         return JSON.parse(window.localStorage.getItem(name));
     } catch (e) {
